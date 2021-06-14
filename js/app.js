255441d048c30d39f4f16e9429ef43fc60c7960f
gilles <sellig.blater@free.com>
2021-06-14 11:20:08 +0200

    Initial commit : mvp

---
+++ js/app.js
@@ -0,0 +1,32 @@
+const body = document.querySelector('body');
+const taImport = document.getElementById('paste-zone__data');
+const btnImport = document.getElementById('btn-import');
+let skus = [];
+const btnPrint = document.getElementById('btn-print');
+
+
+btnImport.addEventListener('click', () => {
+    let importTa = taImport.value.split('\n');
+    console.log(importTa);
+    const finalArray = [];
+    for(line of importTa){
+        if(line.trim() != ''){
+            finalArray.push(line);
+        }
+    }
+    console.log(finalArray);
+    skus = finalArray;
+    alert('Données importées !');
+});
+
+btnPrint.addEventListener('click', () => {
+    body.innerHTML = '';
+    for(var i=0; i<skus.length; i++) {
+        var barcode = document.createElement('img');
+        barcode.id = skus[i];
+        JsBarcode(barcode, skus[i]);
+        barcode.setAttribute('style','margin:1rem;width:300px')
+        body.appendChild(barcode);
+        body.setAttribute('style','display:flex;flex-direction:row;flex-wrap:wrap;align-content:flex-start;')
+    }
+});
